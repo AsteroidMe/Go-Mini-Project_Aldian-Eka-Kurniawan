b28e73908dc3ff67e014e274fe2bcbca512d19f2
Aldian Eka K <aldianeka88@gmail.com>
2024-11-16 11:17:40 +0700

    modifying route

--- route/route.go
+++ route/route.go
@@ -3,6 +3,7 @@ package route
 import (
 	"eco-journal/config"
 	"eco-journal/controller"
+	"eco-journal/middleware"
 	"eco-journal/repository"
 	"eco-journal/service"
 
@@ -16,8 +17,22 @@ func SetupRouter() *gin.Engine {
 	userService := service.NewUserService(userRepo)
 	userController := controller.NewUserController(userService)
 
+	authorRepo := repository.NewAuthorRepository(config.DB)
+	authorService := service.NewAuthorService(authorRepo)
+	authorController := controller.NewAuthorController(authorService)
+
 	r.POST("/register", userController.Register)
 	r.POST("/login", userController.Login)
+
+	authorRoutes := r.Group("/authors")
+	authorRoutes.Use(middleware.AuthMiddleware())
+	{
+		authorRoutes.GET("/", authorController.GetAll)
+		authorRoutes.GET("/:id", authorController.GetDetails)
+		authorRoutes.POST("/", authorController.Create)
+		authorRoutes.PUT("/:id", authorController.Update)
+		authorRoutes.DELETE("/:id", authorController.Delete)
+	}
 
 	return r
 }
